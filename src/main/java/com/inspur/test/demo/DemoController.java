4fd3633f4b5683f840c2d3da532b65396141ca40
AmonJiang <jiangyunfei01@inspur.com>
2023-04-13 20:00:15 +0800

    同步存档
    Signed-off-by: AmonJiang <jiangyunfei01@inspur.com>

--- src/main/java/com/inspur/test/demo/DemoController.java
+++ src/main/java/com/inspur/test/demo/DemoController.java
@@ -1,17 +1,10 @@
 package com.inspur.test.demo;
 
-import com.inspur.test.demo.websocket.WebSocketServer;
-import jdk.nashorn.internal.objects.annotations.Constructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import util.SelfResponse;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.core.MediaType;
-import java.io.IOException;
 import java.util.Map;
 
 @RestController
@@ -55,26 +48,4 @@ public class DemoController {
         return demoService.decimalBattle(input1, input2, runTimes);
 
     }
-
-    @ResponseBody
-    @GetMapping("/page")
-    public ModelAndView page() {
-        return new ModelAndView("webSocket");
-    }
-    /*
-     * description: WebSocket请求
-     * author: jiangyf
-     * date: 2023/4/13 19:22
-     * @param message
-     * @param toUID
-     * @return org.springframework.http.ResponseEntity<java.lang.String>
-     */
-    @RequestMapping("/push/{toUID}")
-    public ResponseEntity<String> pushToClient(String message, @PathVariable String toUID) throws IOException {
-        WebSocketServer.sendInfo(message, toUID);
-        return ResponseEntity.ok("Send Success!");
-    }
-
-
-
 }
